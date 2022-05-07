c4b2082c16af89ee70a6ff2f716f9392cc94976c
PabloGCH <pablochoconi@gmail.com>
2022-05-07 19:08:08 -0300

    Logica del login, se puede loguear y guardar token

---
+++ src/app/service/login-token-request.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginTokenRequestService {
+  tokenURL :string = "http://challenge-react.alkemy.org/"
+  constructor(private http :HttpClient) {
+
+  }
+
+  getToken(email:string, password:string) {
+    const body = {
+      email: email,
+      password: password
+    };
+    return this.http.post<any>(this.tokenURL, body);
+  }
+
+  verifyToken(token :string | null) {
+    return this.http.get(this.tokenURL + token);
+  }
+}
