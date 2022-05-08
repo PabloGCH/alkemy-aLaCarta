a1ceb53a3a4833a6e082802783c50f2c4ce68636
PabloGCH <pablochoconi@gmail.com>
2022-05-07 22:01:43 -0300

    guard para chequear login añadida

---
+++ src/app/guards/login-guard.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuardGuard implements CanActivate {
+  constructor(private _router :Router) {}
+
+  //redirects if it receives a false true
+  redirect(bool :boolean) {
+    if(bool) {
+      this._router.navigate(["login"]);
+    }
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const token = localStorage.getItem("token") === null;
+    this.redirect(token);
+    return token;
+  }
+  
+}
