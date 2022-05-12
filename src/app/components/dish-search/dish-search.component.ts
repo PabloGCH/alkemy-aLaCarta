aae181e74f675d34daf85e75ae3d4aa9ee4d1d3a
PabloGCH <pablochoconi@gmail.com>
2022-05-11 23:39:11 -0300

    dish-search ahora busca platos, dish-box muestra titulo e imagen

--- src/app/components/dish-search/dish-search.component.ts
+++ src/app/components/dish-search/dish-search.component.ts
@@ -1,4 +1,14 @@
+//Dependencies
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs';
+
+//Services
+import { DishRequestService } from 'src/app/service/dish-request.service';
+
+//Components
+import { DishBoxComponent } from '../dish-box/dish-box.component';
+
 
 @Component({
   selector: 'app-dish-search',
@@ -6,10 +16,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dish-search.component.scss']
 })
 export class DishSearchComponent implements OnInit {
-
-  constructor() { }
+  searchbar = new FormControl;
+  dishes :Array<JSON> = [];
+  constructor(private dishRequest :DishRequestService) {
+    /*
+    // Debounce para busqueda
+    this.searchbar.valueChanges.pipe(debounceTime(1200)).subscribe(result => {
+      console.log(result);
+    })
+    */
+  }
+  getDishes() {
+    this.dishRequest.searchDish(this.searchbar.value).subscribe(result => {
+      this.dishes = result.results;
+      console.log(this.dishes);
+    });
+  }
 
   ngOnInit(): void {
+
   }
+
 
 }
